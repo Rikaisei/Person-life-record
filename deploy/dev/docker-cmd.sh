e7ee032df45920126227c5117ee00ab68ef5d0f2
lihuaicehng <huaicheng_a_li@163.com>
2022-04-07 23:55:27 +0800

    eureka 执行脚本

---
+++ deploy/dev/docker-cmd.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+IMAGE_URL=127.0.0.1:5000
+
+NAME=$1
+ENTRY_DIR=`pwd`
+TS=`date '+%Y%m%d%H%M%S'`
+IMAGE_PATH=$IMAGE_URL/life-record-$NAME
+
+echo ENTRY DIR: $ENTRY_DIR
+echo IMAGE PATH: $IMAGE_PATH
+
+cd ../../life-record-$NAME/
+echo ENTRY DIR: `pwd`
+
+docker build -t $IMAGE_PATH .
+docker push $IMAGE_PATH
+docker rmi $IMAGE_PATH
+
+cd $ENTRY_DIR
+echo ENTRY DIR: `pwd`
+echo DOCKER BUILD COMPLETED
+
--- deploy/dev/eureka.sh
+++ deploy/dev/eureka.sh
@@ -1,9 +1,5 @@
-
 NAME=$0
-echo NAME: $NAME
 FILENAME=${NAME##*/}
-echo FILENAME: $FILENAME
 FILENAME_1=${FILENAME%.*}
-echo FILENAME_1: $FILENAME_1
 
-# ./docker-cmd.sh $FILENAME_1
+./docker-cmd.sh $FILENAME_1
