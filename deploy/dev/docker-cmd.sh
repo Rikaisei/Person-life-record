0d21287ea3fb43c074e36b4252eb04f1e8bb6e99
lihuaicehng <huaicheng_a_li@163.com>
2022-04-09 20:58:04 +0800

    docker-cmd.sh update

--- deploy/dev/docker-cmd.sh
+++ deploy/dev/docker-cmd.sh
@@ -15,9 +15,9 @@ echo ENTRY DIR: `pwd`
 
 docker stop life-record-$NAME
 docker rm life-record-$NAME
+docker rmi $IMAGE_PATH
 docker build -t $IMAGE_PATH .
 docker push $IMAGE_PATH
-docker rmi $IMAGE_PATH
 docker run -p 9021:9021 --name life-record-$NAME -d $IMAGE_PATH
 
 cd $ENTRY_DIR
