6ffbd2973e9b1cfcc93a47435c9cd561240b5db9
lihuaicehng <huaicheng_a_li@163.com>
2022-04-09 22:38:12 +0800

    docker-cmd.sh update

--- deploy/dev/docker-cmd.sh
+++ deploy/dev/docker-cmd.sh
@@ -4,7 +4,6 @@ IMAGE_URL=127.0.0.1:5000
 
 NAME=$1
 port=$2
-echo $port
 ENTRY_DIR=`pwd`
 TS=`date '+%Y%m%d%H%M%S'`
 IMAGE_PATH=$IMAGE_URL/life-record-$NAME
@@ -20,7 +19,7 @@ docker rm life-record-$NAME
 docker rmi $IMAGE_PATH
 docker build -t $IMAGE_PATH .
 docker push $IMAGE_PATH
-docker run -p 9021:9021 --name life-record-$NAME -d $IMAGE_PATH
+docker run -p $port:$port --name life-record-$NAME -d $IMAGE_PATH
 
 cd $ENTRY_DIR
 echo ENTRY DIR: `pwd`
